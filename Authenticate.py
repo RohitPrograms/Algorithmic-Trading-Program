59b31985224c71a4c662f01962d8ee5066a55afe
RohitPrograms <33472229+RohitPrograms@users.noreply.github.com>
2022-05-19 22:14:37 -0700

    Added text

--- Authenticate.py
+++ Authenticate.py
@@ -5,3 +5,5 @@ TDSession = TDClient(client_id = CONSUMER_KEY,
                          redirect_uri = REDIRECT_URL,
                          credentials_path = CREDENTIALS_PATH)
 TDSession.login()
+
+# This is just a test to see if the push changes it 
